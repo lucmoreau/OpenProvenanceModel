4269d717537ba417c94ff3690f76dac0d7a9ccf6
Luc Moreau <l.moreau@ecs.soton.ac.uk>
2009-04-07 08:34:15 +0000

    git-svn-id: https://provenance.ecs.soton.ac.uk/contrib-svn/trunk/opm@240 eaacffd3-9a98-4465-91bf-87518ec83039

--- opm/src/main/java/org/openprovenance/model/OPMToDot.java
+++ opm/src/main/java/org/openprovenance/model/OPMToDot.java
@@ -12,7 +12,8 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.JAXBElement;
 import org.w3c.dom.Element;
 
-/** Deserialiser of OPM Graphs. */
+
+/** Serialisation of  OPM Graphs to DOT format. */
 public class OPMToDot {
 
     OPMUtilities u=new OPMUtilities();
@@ -58,31 +59,102 @@ public class OPMToDot {
 
         prelude(out);
 
-        for (Process p: graph.getProcesses().getProcess()) {
-            emitProcess(p,out);
+        if (graph.getProcesses()!=null) {
+            for (Process p: graph.getProcesses().getProcess()) {
+                emitProcess(p,out);
+            }
         }
-        for (Artifact p: graph.getArtifacts().getArtifact()) {
-            emitArtifact(p,out);
+
+        if (graph.getArtifacts()!=null) {
+            for (Artifact p: graph.getArtifacts().getArtifact()) {
+                emitArtifact(p,out);
+            }
+        }
+
+        if (graph.getAgents()!=null) {
+            for (Agent p: graph.getAgents().getAgent()) {
+                emitAgent(p,out);
+            }
         }
 
         for (Edge e: edges) {
-            emitEdge(e,out);
+            emitDependency(e,out);
         }
+        
 
         postlude(out);
        
     }
 
+    //////////////////////////////////////////////////////////////////////
+    ///
+    ///                              NODES
+    ///
+    //////////////////////////////////////////////////////////////////////
+
     public void emitProcess(Process p, PrintStream out) {
-        out.println(p.getId() + "[shape=polygon,sides=4,label=\"" + processLabel(p) + "\"]");
+        HashMap<String,String> properties=new HashMap();
+
+        emitNode(p.getId(),
+                 addProcessShape(p,addProcessLabel(p, properties)),
+                 out);
     }
 
     public void emitArtifact(Artifact a, PrintStream out) {
-        out.println(a.getId() + "[label=\"" + artifactLabel(a) + "\"]");
+        HashMap<String,String> properties=new HashMap();
+
+        emitNode(a.getId(),
+                 addArtifactShape(a,addArtifactLabel(a, properties)),
+                 out);
     }
+
+    public void emitAgent(Agent ag, PrintStream out) {
+        HashMap<String,String> properties=new HashMap();
+
+        emitNode(ag.getId(),
+                 addAgentShape(ag,addAgentLabel(ag, properties)),
+                 out);
+    }
+
+    
+
+    public HashMap<String,String> addProcessShape(Process p, HashMap<String,String> properties) {
+        properties.put("shape","poligon");
+        properties.put("sides","4");
+        return properties;
+    }
+
+    public HashMap<String,String> addProcessLabel(Process p, HashMap<String,String> properties) {
+        properties.put("label",processLabel(p));
+        return properties;
+    }
+
+    public HashMap<String,String> addArtifactShape(Artifact p, HashMap<String,String> properties) {
+        // default is good for artifact
+        return properties;
+    }
+
+    public HashMap<String,String> addArtifactLabel(Artifact p, HashMap<String,String> properties) {
+        properties.put("label",artifactLabel(p));
+        return properties;
+    }
+
+    public HashMap<String,String> addAgentShape(Agent p, HashMap<String,String> properties) {
+        properties.put("shape","poligon");
+        properties.put("sides","5");
+        return properties;
+    }
+
+    public HashMap<String,String> addAgentLabel(Agent p, HashMap<String,String> properties) {
+        properties.put("label",agentLabel(p));
+        return properties;
+    }
+
+
 
     boolean displayProcessValue;
     boolean displayArtifactValue;
+    boolean displayAgentValue;
 
     public String processLabel(Process p) {
         if (displayProcessValue) {
@@ -98,32 +170,12 @@ public class OPMToDot {
             return p.getId();
         }
     }
-
-    public void emitEdge(Edge e, PrintStream out) {
-        List<AccountId> accounts=e.getAccount();
-        if (accounts.isEmpty()) {
-            accounts=new LinkedList();
-            accounts.add(of.newAccountId(of.newAccount(defaultAccountLabel)));
+    public String agentLabel(Agent p) {
+        if (displayAgentValue) {
+            return convertAgentName(""+p.getValue());
+        } else {
+            return p.getId();
         }
-            
-        for (AccountId acc: accounts) {
-            String accountLabel=((Account)acc.getId()).getId();
-            out.print( ((Node)e.getEffect().getId()).getId() + " -> " + ((Node)e.getCause().getId()).getId() + " ");
-            emitEdgeAttributes(accountLabel,e,out);
-        }
-
-    }
-
-    public void emitEdgeAttributes(String accountLabel, Edge e, PrintStream out) {
-        String colour=convertAccount(accountLabel);
-        out.println("[color=\"" + colour + "\", fontcolor=\"" + colour + "\",style=\"" + getEdgeStyle(e) + "\"]");
-    }
-
-    HashMap<String,String> accountColourMap=new HashMap<String,String>();
-    public String convertAccount(String account) {
-        String colour=accountColourMap.get(account);
-        if (colour!=null) return colour;
-        return account;
     }
 
     HashMap<String,String> processNameMap=new HashMap<String,String>();
@@ -138,6 +190,56 @@ public class OPMToDot {
         if (name!=null) return name;
         return artifact;
     }
+    HashMap<String,String> agentNameMap=new HashMap<String,String>();
+    public String convertAgentName(String agent) {
+        String name=agentNameMap.get(agent);
+        if (name!=null) return name;
+        return agent;
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    ///
+    ///                              EDGES
+    ///
+    //////////////////////////////////////////////////////////////////////
+
+    public void emitDependency(Edge e, PrintStream out) {
+        HashMap<String,String> properties=new HashMap();
+
+        List<AccountId> accounts=e.getAccount();
+        if (accounts.isEmpty()) {
+            accounts=new LinkedList();
+            accounts.add(of.newAccountId(of.newAccount(defaultAccountLabel)));
+        }
+            
+        for (AccountId acc: accounts) {
+            String accountLabel=((Account)acc.getId()).getId();
+            addEdgeAttributes(accountLabel,e,properties);
+            emitEdge( ((Node)e.getEffect().getId()).getId(),
+                      ((Node)e.getCause().getId()).getId(),
+                      properties,
+                      out);
+        }
+    }
+
+    public HashMap<String,String> addEdgeAttributes(String accountLabel,
+                                                    Edge e,
+                                                    HashMap<String,String> properties) {
+        String colour=convertAccount(accountLabel);
+        properties.put("color",colour);
+        properties.put("fontcolor",colour);
+        properties.put("style",getEdgeStyle(e));
+        return properties;
+    }
+
+
+    HashMap<String,String> accountColourMap=new HashMap<String,String>();
+    public String convertAccount(String account) {
+        String colour=accountColourMap.get(account);
+        if (colour!=null) return colour;
+        return account;
+    }
 
     String defaultEdgeStyle;
     HashMap<String,String> edgeStyleMap=new HashMap<String,String>();
@@ -150,9 +252,45 @@ public class OPMToDot {
     }
 
     
-
+    //////////////////////////////////////////////////////////////////////
+    ///
+    ///                              DOT FORMAT GENERATION
+    ///
+    //////////////////////////////////////////////////////////////////////
+    
+    
     String name;
     String defaultAccountLabel;
+
+
+    public void emitNode(String name, HashMap<String,String> properties, PrintStream out) {
+        StringBuffer sb=new StringBuffer();
+        sb.append(name);
+        emitProperties(sb,properties);
+        out.println(sb.toString());
+    }
+
+
+    public void emitEdge(String src, String dest, HashMap<String,String> properties, PrintStream out) {
+        StringBuffer sb=new StringBuffer();
+        sb.append(src);
+        sb.append(" -> ");
+        sb.append(dest);
+        emitProperties(sb,properties);
+        out.println(sb.toString());
+    }
+
+    public void emitProperties(StringBuffer sb,HashMap<String,String> properties) {
+        sb.append(" [");
+        for (String key: properties.keySet()) {
+            String value=properties.get(key);
+            sb.append(key);
+            sb.append("=\"");
+            sb.append(value);
+            sb.append("\"");
+        }
+        sb.append("]");
+    }
 
     void prelude(PrintStream out) {
         out.println("digraph " + name + " { rankdir=\"BT\"; ");
