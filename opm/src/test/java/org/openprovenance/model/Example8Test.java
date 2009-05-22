f94987932ebb04128db00bf7ce8f56ea3c619a3b
Luc Moreau <l.moreau@ecs.soton.ac.uk>
2009-05-22 10:48:24 +0000

    git-svn-id: https://provenance.ecs.soton.ac.uk/contrib-svn/trunk/opm@336 eaacffd3-9a98-4465-91bf-87518ec83039

---
+++ opm/src/test/java/org/openprovenance/model/Example8Test.java
@@ -0,0 +1,190 @@
+package org.openprovenance.model;
+import java.io.File;
+import java.io.StringWriter;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import javax.xml.bind.JAXBException;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.openprovenance.model.extension.OPMExtendedFactory;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class Example8Test 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Example8Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+
+
+    static OPMGraph graph1;
+
+
+
+
+    public void testCollectionProposal1() throws JAXBException
+    {
+        OPMExtendedFactory oFactory=new OPMExtendedFactory();
+
+        Collection<Account> black=Collections.singleton(oFactory.newAccount("black"));
+        Collection<Account> orange=Collections.singleton(oFactory.newAccount("orange"));
+        
+        Process p1=oFactory.newProcess("setG",
+                                       black,
+                                       "setG");
+
+
+        Process p2=oFactory.newProcess("append",
+                                       black,
+                                       "append");
+
+
+
+        List<Account> black_orange=new LinkedList();
+        black_orange.addAll(orange);
+        black_orange.addAll(black);
+
+
+        Artifact a1=oFactory.newArtifact("a1",
+                                         black,
+                                         "[a1]");
+        Artifact a2=oFactory.newArtifact("a2",
+                                         black,
+                                         "[a2]");
+        Artifact a3=oFactory.newArtifact("a3",
+                                         black,
+                                         "[a3]");
+
+
+        Artifact g1=oFactory.newArtifact("g1",
+                                         black,
+                                         "g1");
+        Artifact g2=oFactory.newArtifact("g2",
+                                         black,
+                                         "g2");
+
+
+        Artifact f1=oFactory.newArtifact("f1",
+                                         orange,
+                                         "f1");
+        Artifact f2=oFactory.newArtifact("f2",
+                                         orange,
+                                         "f2");
+
+        Artifact f3=oFactory.newArtifact("f3",
+                                         orange,
+                                         "f3");
+
+        Artifact b1=oFactory.newArtifact("b1",
+                                         black,
+                                         "[b1]");
+        Artifact b2=oFactory.newArtifact("b2",
+                                         black,
+                                         "[b2]");
+
+        Artifact b3=oFactory.newArtifact("b3",
+                                         black,
+                                         "[b3]");
+
+
+        Artifact c1=oFactory.newArtifact("c1",
+                                         orange,
+                                         "c1");
+        Artifact c2=oFactory.newArtifact("c2",
+                                         orange,
+                                         "c2");
+
+        Artifact c3=oFactory.newArtifact("c3",
+                                         orange,
+                                         "c3");
+
+
+        
+        Used u1=oFactory.newUsed(p1,oFactory.newRole("old"),a2,black);
+        Used u2=oFactory.newUsed(p1,oFactory.newRole("value"),g1,black);
+
+
+        WasGeneratedBy wg1=oFactory.newWasGeneratedBy(a3,oFactory.newRole("updated"),p1,black);
+
+
+        NamedWasDerivedFrom wd1=oFactory.newNamedWasDerivedFrom(a3,a2,"wasUpdated",black);
+        NamedWasDerivedFrom wd2=oFactory.newNamedWasDerivedFrom(a2,a1,"wasAliasOf",black);
+
+        NamedWasDerivedFrom wd3=oFactory.newNamedWasDerivedFrom(g2,g1,"wasAliasOf",black);
+        NamedWasDerivedFrom wd4=oFactory.newNamedWasDerivedFrom(a3,g2,"contained",black);
+
+        NamedWasDerivedFrom wd5=oFactory.newNamedWasDerivedFrom(f2,f1,"wasAliasOf",orange);
+        NamedWasDerivedFrom wd6=oFactory.newNamedWasDerivedFrom(f3,f2,"wasAliasOf",orange);
+        NamedWasDerivedFrom wd7=oFactory.newNamedWasDerivedFrom(a2,f2,"contained",orange);
+        NamedWasDerivedFrom wd8=oFactory.newNamedWasDerivedFrom(a3,f3,"contained",orange);
+        NamedWasDerivedFrom wd9=oFactory.newNamedWasDerivedFrom(a1,f1,"contained",orange);
+
+
+        NamedWasDerivedFrom wd10=oFactory.newNamedWasDerivedFrom(b2,b1,"wasAppendedTo",black);
+        NamedWasDerivedFrom wd10b=oFactory.newNamedWasDerivedFrom(b2,a1,"wasAppendedTo",black);
+        NamedWasDerivedFrom wd11=oFactory.newNamedWasDerivedFrom(b3,b2,"updated",black);
+
+        NamedWasDerivedFrom wd12=oFactory.newNamedWasDerivedFrom(b2,a2,"contained",black);
+        NamedWasDerivedFrom wd13=oFactory.newNamedWasDerivedFrom(b3,a3,"contained",black);
+
+        NamedWasDerivedFrom wd14=oFactory.newNamedWasDerivedFrom(b1,c1,"contained",orange);
+        NamedWasDerivedFrom wd15=oFactory.newNamedWasDerivedFrom(b2,c2,"contained",orange);
+        NamedWasDerivedFrom wd16=oFactory.newNamedWasDerivedFrom(b3,c3,"contained",orange);
+
+        NamedWasDerivedFrom wd17=oFactory.newNamedWasDerivedFrom(c3,c2,"wasAliasOf",orange);
+        NamedWasDerivedFrom wd18=oFactory.newNamedWasDerivedFrom(c2,c1,"wasAliasOf",orange);
+
+
+
+
+        Used u3=oFactory.newUsed(p2,oFactory.newRole("last"),a1,black);
+        Used u4=oFactory.newUsed(p2,oFactory.newRole("first"),b1,black);
+
+
+        WasGeneratedBy wg2=oFactory.newWasGeneratedBy(b2,oFactory.newRole("result"),p2,black);
+
+
+        OPMGraph graph=oFactory.newOPMGraph(black_orange,
+                                            new Overlaps[] { },
+                                            new Process[] {p1, p2},
+                                            new Artifact[] {a1,a2,a3, g1, g2, f1, f2, f3, b1, b2, b3, c1, c2, c3},
+                                            new Agent[] {  },
+                                            new Object[] {u1,u2,u3, u4,
+                                                          wg1, wg2, 
+                                                          wd1,wd2, wd3, wd4,wd5,wd6,wd7,wd8, wd9, wd10, wd10b, wd11, wd12, wd13, wd14, wd15, wd16, wd17, wd18} );
+
+
+
+
+        OPMSerialiser serial=OPMSerialiser.getThreadOPMSerialiser();
+        serial.serialiseOPMGraph(new File("target/collection1.xml"),graph,true);
+
+        
+        //System.out.println(sw);
+
+        graph1=graph;
+        System.out.println("testOPM1 asserting True");
+        assertTrue( true );
+
+
+        
+    }
+    
+}
---
+++ opm/src/test/java/org/openprovenance/model/Example9Test.java
@@ -0,0 +1,185 @@
+package org.openprovenance.model;
+import java.io.File;
+import java.io.StringWriter;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import javax.xml.bind.JAXBException;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.openprovenance.model.extension.OPMExtendedFactory;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class Example9Test 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Example9Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+
+
+    static OPMGraph graph1;
+
+
+
+
+    public void testCollectionProposal2() throws JAXBException
+    {
+        OPMExtendedFactory oFactory=new OPMExtendedFactory();
+
+        Collection<Account> black=Collections.singleton(oFactory.newAccount("black"));
+        Collection<Account> orange=Collections.singleton(oFactory.newAccount("orange"));
+        
+        List<Account> black_orange=new LinkedList();
+        black_orange.addAll(orange);
+        black_orange.addAll(black);
+
+
+        Process p1=oFactory.newProcess("x",
+                                       black,
+                                       "product");
+
+
+        Process p2=oFactory.newProcess("map",
+                                       black,
+                                       "map");
+
+
+        Process p3=oFactory.newProcess("apply",
+                                       black,
+                                       "apply");
+
+
+
+        Artifact pp=oFactory.newArtifact("pp",
+                                         black,
+                                         "p");
+
+        Artifact a1=oFactory.newArtifact("a",
+                                         black,
+                                         "[a]");
+        Artifact ai1=oFactory.newArtifact("a_i1",
+                                         black,
+                                         "a_i1");
+
+        Artifact ai2=oFactory.newArtifact("a_i2",
+                                         black,
+                                         "a_i2");
+
+
+        Artifact b1=oFactory.newArtifact("b",
+                                         black,
+                                         "[b]");
+        Artifact bi1=oFactory.newArtifact("b_i1",
+                                         black,
+                                         "b_i1");
+
+        Artifact bi2=oFactory.newArtifact("b_i2",
+                                         black,
+                                         "b_i2");
+
+
+        Artifact c1=oFactory.newArtifact("c",
+                                         black,
+                                         "[c1]");
+
+        Artifact cij=oFactory.newArtifact("cij1",
+                                         black,
+                                         "[cij1]");
+
+        Artifact cij2=oFactory.newArtifact("cij2",
+                                         black,
+                                         "cij2");
+
+        Artifact c2=oFactory.newArtifact("c2",
+                                         black,
+                                         "[c2]");
+
+
+        Used u1=oFactory.newUsed(p1,oFactory.newRole("left"),a1,black);
+        Used u2=oFactory.newUsed(p1,oFactory.newRole("right"),b1,black);
+
+
+        WasGeneratedBy wg1=oFactory.newWasGeneratedBy(c1,oFactory.newRole("product"),p1,black);
+
+
+        NamedWasDerivedFrom wd1=oFactory.newNamedWasDerivedFrom(c1,a1,"wasLeftProduct",black);
+        NamedWasDerivedFrom wd2=oFactory.newNamedWasDerivedFrom(c1,b1,"wasRightProduct",black);
+
+        NamedWasDerivedFrom wd3=oFactory.newNamedWasDerivedFrom(ai2,ai1,"wasAliasOf",orange);
+        NamedWasDerivedFrom wd4=oFactory.newNamedWasDerivedFrom(bi2,bi1,"wasAliasOf",orange);
+
+        NamedWasDerivedFrom wd5=oFactory.newNamedWasDerivedFrom(a1,ai1,"contained",orange);
+        NamedWasDerivedFrom wd6=oFactory.newNamedWasDerivedFrom(b1,bi1,"contained",orange);
+
+        NamedWasDerivedFrom wd7=oFactory.newNamedWasDerivedFrom(cij,ai2,"contained",orange);
+        NamedWasDerivedFrom wd8=oFactory.newNamedWasDerivedFrom(cij,bi2,"contained",orange);
+
+        NamedWasDerivedFrom wd9=oFactory.newNamedWasDerivedFrom(c1,cij,"contained",orange);
+
+
+
+
+
+        Used u3=oFactory.newUsed(p2,oFactory.newRole("collection"),c1,black);
+        Used u4=oFactory.newUsed(p2,oFactory.newRole("function"),pp,black);
+
+        WasGeneratedBy wg2=oFactory.newWasGeneratedBy(c2,oFactory.newRole("result"),p2,black);
+
+
+        Used u5=oFactory.newUsed(p3,oFactory.newRole("left"),ai2,orange);
+        Used u6=oFactory.newUsed(p3,oFactory.newRole("right"),bi2,orange);
+        Used u7=oFactory.newUsed(p3,oFactory.newRole("fun"),pp,orange);
+
+        WasGeneratedBy wg3=oFactory.newWasGeneratedBy(cij2,oFactory.newRole("result"),p3,orange);
+
+        NamedWasDerivedFrom wd10=oFactory.newNamedWasDerivedFrom(c2,cij2,"contained",orange);
+
+        NamedWasDerivedFrom wd11=oFactory.newNamedWasDerivedFrom(cij2,cij,"wasApplied",orange);
+
+        NamedWasDerivedFrom wd12=oFactory.newNamedWasDerivedFrom(c2,c1,"wasMapped",black);
+
+
+        OPMGraph graph=oFactory.newOPMGraph(black_orange,
+                                            new Overlaps[] { },
+                                            new Process[] {p1, p2, p3},
+                                            new Artifact[] {a1,ai1, ai2, b1, bi1, bi2, c1, cij, cij2, pp, c2},
+                                            new Agent[] {  },
+                                            new Object[] {u1,u2, u3, u4, u5, u6, u7,
+                                                          wg1,wg2,wg3,
+                                                          wd1, wd2, wd3, wd4, wd5, wd6, wd7, wd8, wd9, wd10, wd11, wd12} );
+
+
+
+
+        OPMSerialiser serial=OPMSerialiser.getThreadOPMSerialiser();
+        serial.serialiseOPMGraph(new File("target/collection2.xml"),graph,true);
+
+        
+        //System.out.println(sw);
+
+        graph1=graph;
+        System.out.println("testOPM1 asserting True");
+        assertTrue( true );
+
+
+        
+    }
+    
+}
