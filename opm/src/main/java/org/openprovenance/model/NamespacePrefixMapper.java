f5b194d301f217144a2b7f21b2e57666527b7943
Luc Moreau <l.moreau@ecs.soton.ac.uk>
2009-10-07 09:39:09 +0000

    git-svn-id: https://provenance.ecs.soton.ac.uk/contrib-svn/opm/trunk@447 eaacffd3-9a98-4465-91bf-87518ec83039

--- opm/src/main/java/org/openprovenance/model/NamespacePrefixMapper.java
+++ opm/src/main/java/org/openprovenance/model/NamespacePrefixMapper.java
@@ -50,10 +50,10 @@ public class NamespacePrefixMapper extends com.sun.xml.bind.marshaller.Namespace
      *      value will be ignored and the system will generate one.
      */
     public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix) {
-        if (namespaceUri.equals("http://openprovenance.org/model/v1.01.a")) {
+        if (namespaceUri.equals("http://openprovenance.org/model/v1.1.a")) {
             return "opm";
         }
-        if (namespaceUri.equals("http://openprovenance.org/model/extension/v1.01.a")) {
+        if (namespaceUri.equals("http://openprovenance.org/model/extension/v1.1.a")) {
             return "opmext";
         }
         if (namespaceUri.equals("http://openprovenance.org/model/opmPrinterConfig")) {
