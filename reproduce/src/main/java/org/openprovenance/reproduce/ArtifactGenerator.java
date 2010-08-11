95875d71d8c803ef678043152e2eea51b157d5ce
Luc Moreau <luc.moreau@uprovenance.com>
2010-08-11 08:18:31 +0100

    reproducibility semantics

--- reproduce/src/main/java/org/openprovenance/reproduce/ArtifactGenerator.java
+++ reproduce/src/main/java/org/openprovenance/reproduce/ArtifactGenerator.java
@@ -1,8 +1,11 @@
 package org.openprovenance.reproduce;
-
+import java.io.StringWriter;
+import java.util.LinkedList;
 import org.openprovenance.model.Artifact;
 import org.openprovenance.model.Process;
 import org.openprovenance.model.OPMFactory;
+import org.openprovenance.model.OPMGraph;
+import org.openprovenance.model.OPMSerialiser;
 
 public class ArtifactGenerator implements ArtifactFactory, ProcessFactory {
     public String artifactPrefix="_a_";
@@ -25,11 +28,22 @@ public class ArtifactGenerator implements ArtifactFactory, ProcessFactory {
     }
 
     public Artifact newArtifact(Artifact a) {
-        // Artifact n=oFactory.newArtifact(a);
-        // n.setId(newArtifactId());
-        // System.out.println(" ==> " + a);
-        // System.out.println(" ==> " + n);
-        return a;
+        Artifact n=oFactory.newArtifact(a);
+        n.setId(newArtifactId());
+        System.out.println(" ==> " + a);
+        System.out.println(" ==> " + n);
+        try {
+            OPMGraph g=oFactory.newOPMGraph(new LinkedList(),new LinkedList(),new LinkedList(),new LinkedList(),null,null);
+            g.getArtifacts().getArtifact().add(a);
+            g.getArtifacts().getArtifact().add(n);
+            OPMSerialiser serial=OPMSerialiser.getThreadOPMSerialiser();
+            StringWriter sw=new StringWriter();
+            serial.serialiseOPMGraph(sw,g,true);
+            System.out.println("$$$$$$$$$$$$$$$ " + sw.getBuffer());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return n;
     }
 
     public Process newProcess(Process a) {
