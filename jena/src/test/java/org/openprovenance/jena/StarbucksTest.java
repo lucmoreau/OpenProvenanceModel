3c41bdf1b49c757b0bda708f5686341f214b5f13
LucMoreau <l.moreau@ecs.soton.ac.uk>
2011-02-25 12:09:08 +0000

    jena

--- jena/src/test/java/org/openprovenance/jena/StarbucksTest.java
+++ jena/src/test/java/org/openprovenance/jena/StarbucksTest.java
@@ -4,6 +4,7 @@ import static org.openprovenance.jena.TripleStore.OPMO_NS;
 import static org.openprovenance.jena.TripleStore.OPMV_NS;
 
 import java.util.List;
+import java.util.LinkedList;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -61,11 +62,21 @@ public class StarbucksTest extends TestCase {
         resources=q.getAgentsAsResources();
         assertTrue((resources!=null) && resources.size()==0);
     }
+    
+    public void checkExpected(List<Resource> resources, List<String> expected, String reason) {
+        System.out.println(reason + ": " + resources);
+        assertTrue(reason, (resources!=null) && (expected!=null) && resources.size()==expected.size());
+        for (Resource res: resources) {
+            String uri=res.getURI();
+            assertTrue(reason, expected.contains(uri));
+        }
+    }
 
 
     public void testEdges() {
 
         List<Resource> resources;
+        List<String> expected;
 
         // The following tests check for the presence of opmv edges
         // inferred from the opmo ontology
@@ -76,29 +87,75 @@ public class StarbucksTest extends TestCase {
         resources=q.getGeneratedArtifactsAsResources("st:p2");
         assertTrue((resources!=null) && resources.size()==1);
 
+        //wdf
+
         resources=q.getDerivedFromArtifactsAsResources("st:a4");
-        System.out.println("Resources1 " + resources);
-        assertTrue((resources!=null) && resources.size()==2);
+        expected=new LinkedList();
+        expected.add("http://example.com/starbucks/a3");
+        expected.add("http://example.com/starbucks/a6");
+        checkExpected(resources,expected,"check wdf from a4");
 
         resources=q.getDerivedFromArtifactsAsResources("st:a6");
-        System.out.println("Resources1a " + resources);
-        assertTrue((resources!=null) && resources.size()==2);
-
+        expected=new LinkedList();
+        expected.add("http://example.com/starbucks/a2");
+        expected.add("http://example.com/starbucks/a1");
+        checkExpected(resources,expected,"check wdf from a6");
 
         resources=q.getDerivedFromArtifactsAsResources("st:a3");
-        System.out.println("Resources1b " + resources);
-        assertTrue((resources!=null) && resources.size()==1);
+        expected=new LinkedList();
+        expected.add("http://example.com/starbucks/a1");
+        checkExpected(resources,expected,"check wdf from a3");
 
+
+        resources=q.getDerivedFromArtifactsAsResources("st:a5");
+        expected=new LinkedList();
+        expected.add("http://example.com/starbucks/a1");
+        checkExpected(resources,expected,"check wdf from a5");
+
+        resources=q.getDerivedFromArtifactsAsResources("st:a1");
+        expected=new LinkedList();
+        checkExpected(resources,expected,"check wdf from a1");
+
+        // wdf*
+
+        resources=q.getDerivedFromStarArtifactsAsResources("st:a6");
+        expected=new LinkedList();
+        expected.add("http://example.com/starbucks/a2");
+        expected.add("http://example.com/starbucks/a1");
+        checkExpected(resources,expected,"check wdf* from a6");
+
+
+        resources=q.getDerivedFromStarArtifactsAsResources("st:a3");
+        expected=new LinkedList();
+        expected.add("http://example.com/starbucks/a1");
+        checkExpected(resources,expected,"check wdf* from a3");
+
+        resources=q.getDerivedFromStarArtifactsAsResources("st:a1");
+        expected=new LinkedList();
+        checkExpected(resources,expected,"check wdf* from a1");
 
         resources=q.getDerivedFromStarArtifactsAsResources("st:a4");
-        System.out.println("Resources2 " + resources);
-        System.out.println("Resources2 ***** ********************************************************** NOT CORRECT!");
-        assertTrue((resources!=null) && resources.size()==2);
+        expected=new LinkedList();
+        expected.add("http://example.com/starbucks/a6");
+        expected.add("http://example.com/starbucks/a3");
+        checkExpected(resources,expected,"==> PROBLEM WITH wdf* from a4");
+        System.out.println("==> NOTE how in the previous example a2 and a1 were not found");
+
+
+
+        // used*
 
 
         resources=q.getUsedStarArtifactsAsResources("st:p2");
-        System.out.println("Resources3 " + resources);
-        assertTrue((resources!=null) && resources.size()==4);
+        expected=new LinkedList();
+        expected.add("http://example.com/starbucks/a6");
+        expected.add("http://example.com/starbucks/a3");
+        expected.add("http://example.com/starbucks/a1");
+        expected.add("http://example.com/starbucks/a2");
+        checkExpected(resources,expected,"check used* from p2");
+
+
+        //
 
         ResultSet results=q.getCauseWasDerivedFromArtifacts("st:a3");
         ResultSetFormatter.out(System.out, results);
